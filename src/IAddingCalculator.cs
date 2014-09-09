0d69bbe6bd447f565bcb642a05f27fb8055f5d2c
Dariusz Wozniak <dariusz.wozniak@gmail.com>
2014-09-09 10:25:24 +0200

    added sandbox

--- src/IAddingCalculator.cs
+++ src/IAddingCalculator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics.Contracts;
+using NUnit.Framework;
 
 namespace DesignByContract
 {
@@ -50,6 +51,25 @@ namespace DesignByContract
         private void CheckIfLastResultIsInRange()
         {
             Contract.Invariant(_lastResult >= 0);
+        }
+    }
+
+    [TestFixture]
+    class CalculatorSandbox
+    {
+        [Test, Ignore]
+        public void Playground()
+        {
+            var calculator = new AddingCalculatorCodeContracts();
+            // CASE 1: Broke preconditions
+            // calculator.Add(-1, 0);
+
+            // CASE 2: Broke object invariants
+            // calculator.Add(Int32.MaxValue, 1);
+
+            // CASE 3: Remember to include Contract.EndContractBlock();
+            //var oldCalculator = new AddingCalculator();
+            //oldCalculator.Add(-1, 0);
         }
     }
 }
